55e1ec22debaa773c08e73d58772b82d07b6bbc2
Dohyun Kim <dkmqflx@gmail.com>
2023-03-26 23:21:58 +0900

    4-3) Layout Shift 피하기

--- part2/lecture-4/src/components/PhotoItem.js
+++ part2/lecture-4/src/components/PhotoItem.js
@@ -17,11 +17,24 @@ function PhotoItem({ photo: { urls, alt } }) {
   );
 }
 
-const ImageWrap = styled.div``;
+const ImageWrap = styled.div`
+  width: 100%;
+  padding-bottom: 56.25%;
+  position: relative;
+  /* 
+  9/16 = 56.25% 
+  너비의 56.25% 만큼 padding을 세팅해라 
+  */
+`;
 
 const Image = styled.img`
   cursor: pointer;
+  position: absolute;
   width: 100%;
+  height: 100%;
+  /* height: 56.25%; */
+  top: 0;
+  left: 0;
 `;
 
 export default PhotoItem;
